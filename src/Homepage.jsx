641fd8e96841d733cffb9b2803bf2262bb2cef59
BellaAkins <sarabellaakins@gmail.com>
2025-04-08 17:22:51 +0100

    add error handling

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
@@ -7,17 +6,30 @@ const API_URL = "https://www.omdbapi.com?apikey=561bc012";
 
 const Homepage = () => {
   const [theMovies, setTheMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-    setTheMovies(data.Search);
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      if (data.Response === "True") {
+        setTheMovies(data.Search);
+      } else {
+        setTheMovies([]);
+      }
+    } catch (error) {
+      console.error("Failed to fetch movies:", error);
+      setTheMovies([]);
+    }
   };
 
   useEffect(() => {
     searchMovies("Barbie");
   }, []);
+
   return (
     <div className="homepage">
       <h1>MovieSphere</h1>
@@ -26,15 +38,17 @@ const Homepage = () => {
           type="text"
           placeholder="Search for movies"
           value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt="search"
           onClick={() => {
-            searchMovies(searchTerm);
+            if (searchTerm.trim() !== "") {
+              searchMovies(searchTerm);
+            } else {
+              console.error("Search term is empty");
+            }
           }}
         />
       </div>
